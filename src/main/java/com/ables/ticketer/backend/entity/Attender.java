119eecdbe22cd70992e8bfc870558545c169360e
crudables <crudables@yahoo.com>
2020-09-09 06:04:13 +0100

    testing eclipse git

--- src/main/java/com/ables/ticketer/backend/entity/Attender.java
+++ src/main/java/com/ables/ticketer/backend/entity/Attender.java
@@ -2,6 +2,9 @@ package com.ables.ticketer.backend.entity;
 
 import java.util.List;
 
+import javax.persistence.OneToMany;
+
+
 
 
 
@@ -11,7 +14,7 @@ public class Attender extends Entity{
 	private String lastName;
 	private String email;
 	private String phone;
-	
-	
+
+	@OneToMany
 	private List<Event> myEvent;
 }
